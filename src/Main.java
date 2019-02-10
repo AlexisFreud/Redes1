f5d6baaa2a292490cfbaed6bba9f0bbfcd581cfa
Luis Fernando Wilson Elias <luisfwilson1998@gmail.com>
2019-02-09 19:18:39 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,35 +17,5 @@ public class Main{
         transmisor.setPanel(panelT);
         receptor.setPanel(panelR);
         
-        
-        
-        
     }
-        /*
-            //Main m = new Main();
-            char[] Array = {'a', 'b', 'c', 'd', 'e',
-                             'f', '0', '1', '2', '3',
-                             '4', '5', '6', '7', '8', 
-                             '9'};
-            for(char c: Array) {
-                    int[] juan = Traductor.traducir(c);
-                    for(int i: juan) {
-                            System.out.print(i + " ");
-                    }
-                    System.out.print(Traductor.traducir(juan));
-                    Timer.esperar(true);
-                    System.out.println();
-            }
-    }
-
-    public Main(){
-        /*
-            super("Redes");
-            this.setPreferredSize(new Dimension(1280, 720));
-            //this.add(new PanelR());
-            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            this.pack();
-            this.setVisible(true);
-
-    }*/
 }
